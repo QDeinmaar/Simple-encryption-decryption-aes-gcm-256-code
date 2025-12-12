9a57660dbadaa336539dd65bfcd4fe687a81b6da
Youness Ben <tudorbenhadda@gmail.com>
2025-12-13 00:19:24 +0100

    working on decryption

--- bin.c
+++ bin.c
@@ -1,11 +1,10 @@
 #include "Headers.h"
 
 
-void save_encrypted(uint8_t *IV, size_t IV_len,
-                    uint8_t TAG, size_t Tag_len,
-                    uint8_t Add, size_t Add_len,
-                    uint8_t salt, size_t salt_len,
-                    uint8_t ciphertext, size_t ciphertext_len
+void save_encrypted(uint8_t *IV,
+                    uint8_t *TAG,
+                    uint8_t *Add, size_t Add_len,
+                    uint8_t *ciphertext, size_t ciphertext_len
                      ){
 
 FILE *fp = fopen("Bin", "wb");
@@ -17,19 +16,69 @@ if (!fp)
 } else{
      // writing the iv, tag, add...
 
-    fwrite(IV, 1, IV_len, fp);
+    if(fwrite(IV, 1, 12, fp) != 12){
 
-    fwrite(TAG, 1, Tag_len, fp);
+        perror("Failed to write IV !");
+        fclose(fp);
+        return;
+    }
+
+    if(fwrite(TAG, 1, 16, fp) != 16){
+
+        perror(" Failed to write TAG !");
+        fclose(fp);
+        return;
+    }
     
-    fwrite(Add, 1, Add_len, fp);
+    if(fwrite(Add, 1, Add_len, fp) != Add_len){
 
-    fwrite(salt, 1, salt_len, fp);
+        perror("Failed to write Add !");
+        fclose(fp);
+        return;
+    }
 
-    fwrite(ciphertext, 1, ciphertext_len, fp);
+    if(fwrite(ciphertext, 1, ciphertext_len, fp) != ciphertext_len){
+
+        perror("Failed to write cipher !");
+        fclose(fp);
+        return;
+    }
 
     fclose(fp);
 }
-
-
 }
 
+void Fd_key(uint8_t *Key, uint8_t *salt){
+    FILE *fkey = fopen("Kbin", "wb");
+
+    if(!fkey)
+    {
+        perror("fopen");
+        return;
+    } else{
+
+       if(fwrite(Key, 1, 32, fkey) != 32){
+
+        perror("Key deviration Failed !");
+        fclose(fkey);
+        return;
+       }
+
+       if(fwrite(salt, 1, 16, fkey) != 16){
+
+        perror("Salt Failed !");
+        fclose(fkey);
+        return;
+       }
+   
+    }
+    
+    fclose(fkey);
+}
+
+uint8_t read_fp(const char *fp, uint8_t *IV,
+                uint8_t Tag,
+                uint8_t *Add, size_t Add_len,
+                uint8_t *ciphertext, size_t ciphertext_len){
+
+                }
